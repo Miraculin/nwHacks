b4ebb16e06dd8720419ec84975b95206209eea28
athonly@gmail.com <athonly@gmail.com>
2019-01-27 02:12:50 -0800

    Update Parser

--- TextConvert.py
+++ TextConvert.py
@@ -1,4 +1,12 @@
 from textblob import *
+# from bs4 import BeautifulSoup
+import wikipedia
+from grammarbot import GrammarBotClient
+
+string = wikipedia.summary("Burj Khalifa")
+
+if not string:
+    print("Wiki Sum Fail")
 
 def paragraph(para):
     blob = TextBlob(para)
@@ -9,27 +17,68 @@ def paragraph(para):
                 retList.append(sentence)
     return retList
 
+
 def sentenceToQuestion(list):
-    nouns = []
     questionlist = []
+    answerlist = []
+    result = []
+    client = GrammarBotClient()
+
     for sen in list:
-        index = 1
-        question = ""
-
         tags = sen.tags
-        #print(tags)
+        question = ""
+        answer = ""
 
-        if tags[0][1] == "PRP":
-            question = "What"
-            while index < len(tags):
-                question += " " + tags[index][0]
-                index += 1
-        print(question)
+        # if tags[0][1] == "PRP":
+        #     question = createQuestion(tags, 1)
 
-        if not question:
-            questionlist.append(question)
-    return questionlist
+        if tags[0][1] == "DT":
+            if tags[1][1][0] == "N" and not tags[2][1] == "IN":
+                answer = tags[1][0]
+                tagindex = 1
+                while tags[tagindex][1][0] == "N" and tagindex < len(tags):
+                    answer += " " + tags[tagindex][0]
+                    tagindex += 1
+
+                tagindex -= 1
+                index = sen.find(tags[tagindex][0]) + len(tags[tagindex][0])
+                question = createQuestion(sen, index)
+
+        if tags[0][1][0] == "N":
+            answer = tags[0][0]
+            tagindex = 1
+            while tags[tagindex][1][0] == "N" and tagindex < len(tags):
+                answer += " " + tags[tagindex][0]
+                tagindex += 1
+
+            tagindex -= 1
+            index = sen.find(tags[tagindex][0]) + len(tags[tagindex][0])
+            question = createQuestion(sen, index)
+
+        if question:
+            if question.find(answer) == -1:
+                print(sen)
+                print(question)
+                print(answer)
+
+                print(client.check(question))
+
+                questionlist.append(question)
+                answerlist.append(answer)
+
+    result.append(questionlist)
+    result.append(answerlist)
+    return result
+
+def createQuestion(sentence, index):
+    question = "What"
+
+    question += " " + str(sentence[index + 1:-1])
+    index += 1
+
+    return question
 
 asdf = "Aaron is the first man on Earth."
 
-sentenceToQuestion(paragraph(asdf))
+# print (paragraph(string))
+print ( sentenceToQuestion(paragraph(string)) )
