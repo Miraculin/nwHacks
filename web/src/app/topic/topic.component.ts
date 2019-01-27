ce29a2005f168a77c0aa5e148b6645b08db3f7bf
Aaron Huang <aaronhuangwork@gmail.com>
2019-01-27 02:51:55 -0800

    ground work for category buttons

--- web/src/app/topic/topic.component.ts
+++ web/src/app/topic/topic.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-topic',
@@ -8,10 +9,20 @@ import { Router } from '@angular/router';
 })
 export class TopicComponent implements OnInit {
   private topic: String;
+  private topicButtons: String[];
+  private url = 'http://127.0.0.1:5000/categories';
 
-  constructor(public router: Router) { }
+  constructor(
+    private router: Router,
+    private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get(this.url).subscribe(
+      (res: any) => {
+        this.topicButtons = res.body;
+        console.log(this.topicButtons);
+      }
+    );
   }
 
   onEnter(value: String) {
