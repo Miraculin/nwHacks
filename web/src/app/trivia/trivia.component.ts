70f7853e5bfa00e44f3d2191d3062791d1ff6f8b
Aaron Huang <aaronhuangwork@gmail.com>
2019-01-27 08:54:58 -0800

    questions now show from rest endpoint

--- web/src/app/trivia/trivia.component.ts
+++ web/src/app/trivia/trivia.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TopicService } from '../topic.service';
 
@@ -8,8 +8,8 @@ import { TopicService } from '../topic.service';
   styleUrls: ['./trivia.component.css']
 })
 export class TriviaComponent implements OnInit {
-  private questions: String[];
-  private answers: String[];
+  private questions: String[] = [];
+  private answers: String[] = [];
   private currentQ: String = 'No questions found';
   private url = 'http://127.0.0.1:5000/trivia/';
 
@@ -19,11 +19,18 @@ export class TriviaComponent implements OnInit {
     this.http.get(this.url + this.service.getTopic()).subscribe(
       (res: any) => {
         console.log(res);
-        if (res[0].length !== -1) {
-          this.questions = res[0];
-          this.currentQ = this.questions[0];
-          this.answers = res[1];
+        for (const i of res) {
+          if (res.indexOf(i) % 2 === 0) {
+            this.questions = this.questions.concat(i);
+          } else {
+            this.answers = this.answers.concat(i);
+          }
         }
+        if (this.questions.length !== 0) {
+          this.currentQ = this.questions[0];
+        }
+        console.log(this.questions);
+        console.log(this.answers);
       }
     );
   }
@@ -32,8 +39,8 @@ export class TriviaComponent implements OnInit {
     if (this.answers.indexOf(ans) !== -1) {
       console.log('ya');
       if (this.questions.length !== 0) {
-        this.questions = this.questions.slice(0);
-        this.currentQ = this.questions[0];
+        console.log('aetaewt');
+        this.currentQ = this.questions.shift();
       } else {
         this.currentQ = 'No more questions';
       }
