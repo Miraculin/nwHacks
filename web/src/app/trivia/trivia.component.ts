ca0bb784dff7544a7dc10c581107c23bd90c6820
Aaron Huang <aaronhuangwork@gmail.com>
2019-01-27 05:24:44 -0800

    implementing endpoints in trivia

--- web/src/app/trivia/trivia.component.ts
+++ web/src/app/trivia/trivia.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TopicService } from '../topic.service';
 
 @Component({
   selector: 'app-trivia',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trivia.component.css']
 })
 export class TriviaComponent implements OnInit {
+  private questions: String[];
+  private answers: String[];
+  private currentQ: String = 'YEET';
+  private url = 'http://127.0.0.1:5000/trivia/';
 
-  constructor() { }
+  constructor(private http: HttpClient, private service: TopicService) { }
 
   ngOnInit() {
+    this.http.get(this.url + this.service.getTopic()).subscribe(
+      (res: any) => {
+        console.log(res);
+      }
+    );
   }
 
+  onEnter(ans: String) {
+    //
+  }
 }
